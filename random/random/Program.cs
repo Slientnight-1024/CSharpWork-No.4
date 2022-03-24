12d0924228aeb9e8d4e8d55395c444d6c459f435
CSharpWork <826815676@qq.com>
2022-03-24 15:48:17 +0800

    添加项目文件。

---
+++ random/random/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class random{
+    static void Main(string[] args){
+        int i;
+        double sum = 0;
+        int[] arr = new int[100];
+        Random random = new Random();
+        for (i = 0; i < arr.Length; i++){
+            arr[i] = random.Next(1000) + 1;
+        }
+            arr = arr.Select(x => x).OrderByDescending(x => x).ToArray();
+        for (i = 0; i < arr.Length; i++){
+            Console.WriteLine("{0:0.000}", arr[i]);
+            sum += arr[i];
+        }
+        Console.WriteLine("求和：{0:0.000}", sum);
+        Console.WriteLine("平均值：{0:0.000}", sum / 100);
+    }
+}
